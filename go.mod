9d1436090c5f6aaa184dd4f825da8ad42ca3738a
Eli Gundry <eligundry@gmail.com>
2021-06-09 19:19:12 -0400

    Wire up fetching of page speed scores

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/eligundry/page-speed-shield
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.7.2
+require (
+	github.com/canthefason/go-watcher v0.2.4 // indirect
+	github.com/fatih/color v1.12.0 // indirect
+	github.com/gin-gonic/gin v1.7.2
+	google.golang.org/api v0.48.0
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+)
--- main.go
+++ main.go
@@ -2,18 +2,60 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/option"
+	"google.golang.org/api/pagespeedonline/v5"
 )
 
+var validInsightTypes = map[string]bool{
+	"mobile":  true,
+	"desktop": true,
+}
+
 func handler(c *gin.Context) {
+	url := c.Param("url")[1:]
+	insightType := c.Param("insightType")
+
+	if _, ok := validInsightTypes[insightType]; !ok {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "insightType must be either mobile or desktop",
+		})
+		return
+	}
+
+	pageSpeedSvc, err := pagespeedonline.NewService(
+		c.Request.Context(),
+		option.WithAPIKey(os.Getenv("GOOGLE_API_KEY")),
+	)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	res, err := pageSpeedSvc.Pagespeedapi.Runpagespeed(url).Do()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"hello": "world",
+		"insightType": insightType,
+		"url":         url,
+		"pagespeed":   res.LighthouseResult.Categories.Performance.Score,
 	})
+	// c.Redirect(http.StatusTemporaryRedirect, "https://img.shields.io/badge/Page%20Speed%20Insights-99-green")
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/*url", handler)
+	router.GET("/:insightType/*url", handler)
 	router.Run()
 }
